6dfcd64e57a855c36e4db2b763098dd92b89165e
Æscanor <113015001+Aescanor@users.noreply.github.com>
2023-08-28 17:08:27 +0200

    test route protected create => ok

--- server/routes/exerciseRoutes.js
+++ server/routes/exerciseRoutes.js
@@ -9,12 +9,17 @@ const getExercisesByParam = require('../middleware/paramMiddleware');
 // router.post('/', exerciceController.createExercise);
 // 01.b Ajouter un exercice protéger une route avec le middleware :
 router.post('/', authMiddleWare, exerciceController.createExercise);
+
 // 02.a Afficher tous les exercices :
 // router.get('/', exerciceController.getAllExercises);
 // 02.b Route protégée avec le middleware : 
 router.get('/', authMiddleWare, exerciceController.getAllExercises);
-// 03. Créer une table d'exercices aléatoires:
-router.get('/random', exerciceController.getRandomRoutine);
+
+// 03.a Créer une table d'exercices aléatoires:
+router.get('/random', authMiddleWare, exerciceController.getRandomRoutine);
+// 03.b Créer une table d'exercices aléatoires:
+// router.get('/random', exerciceController.getRandomRoutine);
+
 // 04. Afficher un exercice par son id :
 router.get('/:id', exerciceController.getExerciseById);
 // 05. Modifier un exercice :
