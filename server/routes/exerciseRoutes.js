c243a46612f4e4a5c0b4399d13a78ee8a1263a5d
Æscanor <113015001+Aescanor@users.noreply.github.com>
2023-08-28 18:12:30 +0200

    test route protected updatedById => ok

--- server/routes/exerciseRoutes.js
+++ server/routes/exerciseRoutes.js
@@ -5,30 +5,28 @@ const authMiddleWare = require('../middleware/authMiddleWare');
 const getExercisesByParam = require('../middleware/paramMiddleware'); 
 
 // Routes :
-// 01.a Ajouter un exercice :
-// router.post('/', exerciceController.createExercise);
-// 01.b Ajouter un exercice protéger une route avec le middleware :
+// 01. Ajouter un exercice - Create Exercise:
 router.post('/', authMiddleWare, exerciceController.createExercise);
+// router.post('/', exerciceController.createExercise);
 
-// 02.a Route getAllExercises protégée avec le middleware : 
+// 02.a Route getAllExercises - Afficher tous les exercices : 
 router.get('/', authMiddleWare, exerciceController.getAllExercises);
-// 02.b Afficher tous les exercices :
 // router.get('/', exerciceController.getAllExercises);
 
-// 03.a Route random protégée avec le middleware - Créer une table d'exercices aléatoires:
+// 03. Route getRandomRoutine - Créer une table d'exercices aléatoires:
 router.get('/random', authMiddleWare, exerciceController.getRandomRoutine);
-// 03.b Créer une table d'exercices aléatoires:
 // router.get('/random', exerciceController.getRandomRoutine);
 
-// 04.a Afficher un exercice par son id :
+// 04. Afficher un exercice par son id - Read Exercise by id:
 router.get('/:id', authMiddleWare, exerciceController.getExerciseById)
-// 04.b Afficher un exercice par son id :
 // router.get('/:id', exerciceController.getExerciseById);
 
-// 05.a Modifier un exercice :
-router.put('/:id', exerciceController.updateExercise);
+// 05. Modifier un exercice - Update Exercise:
+router.put('/:id', authMiddleWare, exerciceController.updateExercise);
+// router.put('/:id', exerciceController.updateExercise);
+
 // 06. Supprimer un exercice :
-router.delete('/:id', exerciceController.deleteExercise);
+// router.delete('/:id', exerciceController.deleteExercise);
 
 // 07. Afficher les exercices par type:
 // Utilisation du middleware pour les paramètres variables
